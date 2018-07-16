ca86cf83bbfbe641ab0da25f20ac2d8b523ff765
Tsubasa HIRANO <t.hirano@fluffy-bunnys.jp>
2018-07-16 18:32:56 +0900

    add sample code

---
+++ assets/first_step_codes/JAZUG.Sample/JAZUG.Sample.Core/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace JAZUG.Sample.Core
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ assets/first_step_codes/JAZUG.Sample/JAZUG.Sample.Mac/AppDelegate.cs
@@ -0,0 +1,33 @@
+﻿using AppKit;
+using Foundation;
+using JAZUG.Sample.Core;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace JAZUG.Sample.Mac
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : FormsApplicationDelegate
+    {
+        private NSWindow _window;
+
+        public AppDelegate()
+        {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+            var rect = new CoreGraphics.CGRect(200, 1000, 400, 600);
+            this._window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+        }
+
+        public override NSWindow MainWindow => this._window;
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            Forms.Init();
+            LoadApplication(new App());
+            base.DidFinishLaunching(notification);
+        }
+
+        public override bool ApplicationShouldTerminateAfterLastWindowClosed(NSApplication sender) => true;
+    }
+}
